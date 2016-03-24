c7c826009559dce89e594981e4e97048bd79313a
SalvatierraFran <franco14_93@hotmail.com>
2016-03-24 00:26:18 -0300

    Cuadrado y cubo de un numero.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            double cuadrado;
+            double cubo;
+
+            Console.Write("Ingrese numero: ");
+            num = int.Parse(Console.ReadLine());
+
+            cuadrado = Math.Pow(num, 2);
+            cubo = Math.Pow(num, 3);
+
+            Console.Write("\nSu cuadrado es " + cuadrado);
+            Console.Write("\nSu cubo es " + cubo);
+
+            Console.ReadKey();
+        }
+    }
+}
